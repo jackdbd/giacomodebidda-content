6d6df7d4d1a097f41d7a20e474aa75df4aa4e399
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-10-17 22:45:38 +0200

    add devenv scripts

--- devenv.nix
+++ devenv.nix
@@ -10,10 +10,12 @@
   '';
 
   env = {
-    GREET = "devenv";
+    PATH_TO_REMOVE_FROM_GIT_HISTORY = "media/photos/2024/10/17/img-20230210-135327.jpg";
   };
 
-  languages.nix.enable = true;
+  languages = {
+    nix.enable = true;
+  };
 
   packages = with pkgs; [
     git
@@ -21,9 +23,25 @@
     git-filter-repo
   ];
 
-  pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks = {
+    shellcheck.enable = true;
+  };
 
   scripts = {
+    largest-blobs.exec = ''
+      git filter-repo --analyze --force
+      cat .git/filter-repo/analysis/blob-shas-and-paths.txt | head -n 7
+    '';
+
+    remove-file-from-history.exec = ''
+      git filter-repo --path ${config.env.PATH_TO_REMOVE_FROM_GIT_HISTORY} --invert-paths
+    '';
+
+    repo-size.exec = ''
+      git gc
+      git count-objects --human-readable --verbose
+    '';
+
     versions.exec = ''
       echo "=== Versions ==="
       git --version
