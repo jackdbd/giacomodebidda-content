1bc6d1ad70e18e09e4bbeaead3ed4c12f3cbea99
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-10-20 19:13:10 +0200

    add markserv to test markdown files in the browser

--- devenv.nix
+++ devenv.nix
@@ -20,25 +20,35 @@
   pre-commit.hooks = {
     alejandra.enable = true;
     deadnix.enable = true;
+    denofmt.enable = true;
+    html-tidy.enable = true;
+    # lychee.enable = true;
+    markdownlint.enable = true;
     shellcheck.enable = true;
     statix.enable = true;
+    typos = {
+      enable = true;
+      excludes = [
+        "articles/lorem-ipsum.md"
+      ];
+    };
   };
 
   scripts = {
+    articles.exec = "npx markserv articles";
     largest-blobs.exec = ''
       git filter-repo --analyze --force
       cat .git/filter-repo/analysis/blob-shas-and-paths.txt | head -n 7
     '';
-
+    notes.exec = "npx markserv notes";
     repo-size.exec = ''
       git gc
       git count-objects --human-readable --verbose
     '';
-
+    serve.exec = "npx markserv .";
     serve-media.exec = ''
       python3 -m http.server --directory media 8080
     '';
-
     versions.exec = ''
       echo "=== Versions ==="
       git --version
